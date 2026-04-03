f73eb06a40dbe309a823df86c4f0faa5dcc37203
0chul <0chul@users.noreply.github.com>
2026-04-03 04:25:16 +0000

    deploy: 378a2a4455f0c7f4287f422f71f4e602aecf1778

--- assets/dateUtils-bwg4b33L.js
+++
@@ -1,2 +0,0 @@
-const r=o=>{const t=new Date(o),e=Math.floor((new Date().getTime()-t.getTime())/1e3);return e<60?"방금 전":e<3600?`${Math.floor(e/60)}분 전`:e<86400?`${Math.floor(e/3600)}시간 전`:e<2592e3?`${Math.floor(e/86400)}일 전`:t.toLocaleDateString("ko-KR",{year:"numeric",month:"long",day:"numeric"})};export{r as g};
-//# sourceMappingURL=dateUtils-bwg4b33L.js.map
